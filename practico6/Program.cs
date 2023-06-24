9c8ba8e51f49ddfbafc6c2cddc36eeeda9892077
Gianfranco Bernachi <gianfrancobernachi@gmail.com>
2023-06-24 20:21:23 -0300

    Primer commit

---
+++ practico6/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);*/
+//Punto1
+/*Console.WriteLine("Escriba un numero:");
+if (float.TryParse(Console.ReadLine(),out float num))
+{
+    if (num > 0)
+    {
+        float invertido = (1/num);
+        Console.WriteLine("El numero invertido es: "+invertido);
+    }
+}
+else
+{
+    Console.WriteLine("No ingresaste un numero");
+}*/
