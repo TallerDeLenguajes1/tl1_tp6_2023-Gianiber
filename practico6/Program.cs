7bcf5df6f2234fcde503b53b68d83fc58451a607
Gianfranco Bernachi <gianfrancobernachi@gmail.com>
2023-06-24 20:38:49 -0300

    CalculadoraV1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -19,3 +19,48 @@ else
 {
     Console.WriteLine("No ingresaste un numero");
 }*/
+
+//Punto2
+/*int a, r, b, i = 1;
+while (i != 0)
+{
+    Console.WriteLine("Seleccione la operacion que desea realizar:");
+    Console.WriteLine("1-Sumar");
+    Console.WriteLine("2-Restar");
+    Console.WriteLine("3-Multiplicar");
+    Console.WriteLine("4-Dividir");
+    r = int.Parse(Console.ReadLine());
+    //int.TryParse(Console.ReadLine(),out r);
+    switch (r)
+    {
+        case 1:
+            Console.WriteLine("Ingrese 2 numeros");
+            int.TryParse(Console.ReadLine(),out a);
+            int.TryParse(Console.ReadLine(),out b);
+            Console.WriteLine("El resultado es:" + (a + b));
+            break;
+        case 2:
+            Console.WriteLine("Ingrese 2 numeros");
+            int.TryParse(Console.ReadLine(),out a);
+            int.TryParse(Console.ReadLine(),out b);
+            Console.WriteLine("El resultado es:" + (a - b));
+            break;
+        case 3:
+            Console.WriteLine("Ingrese 2 numeros");
+            int.TryParse(Console.ReadLine(),out a);
+            int.TryParse(Console.ReadLine(),out b);
+            Console.WriteLine("El resultado es:" + (a * b));
+            break;
+        case 4:
+            Console.WriteLine("Ingrese 2 numeros");
+            int.TryParse(Console.ReadLine(),out a);
+            int.TryParse(Console.ReadLine(),out b);
+            Console.WriteLine("El resultado es:" + (a / b));
+            break;
+        default:
+            Console.WriteLine("No ingresaste una opcion valida");
+            break;
+    }
+    Console.WriteLine("¿Desea seguir operando?(Si=1 - No=0)");
+    int.TryParse(Console.ReadLine(),out i);
+}*/
